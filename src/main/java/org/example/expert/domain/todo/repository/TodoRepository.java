f8f6a994d2969663366d6a80e9876f0c2d4665cd
kiback <rlqor968@gmail.com>
2025-04-21 11:23:42 +0900

    레벨2 2: EntityGraph 누락 수정: TodoRepository 내 메서드  ㅎ하나 EntityGraph 처ë¦미처리 되어 있어 처리하였음

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -17,9 +17,11 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query("SELECT t FROM Todo t ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
-            "WHERE t.id = :todoId")
+    //@Query("SELECT t FROM Todo t " +
+    //        "LEFT JOIN FETCH t.user " +
+    //        "WHERE t.id = :todoId")
+    @EntityGraph(attributePaths = "user")
+    @Query("SELECT t from Todo t WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
